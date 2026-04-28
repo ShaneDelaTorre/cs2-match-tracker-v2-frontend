8328432ddf989ef03e4aa44d8f9f468aa4436c56
Shane <jc.delatorre@academy.opswerks.com>
2026-04-28 22:19:52 +0800

    chore: update landing page to redirect to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
+import { redirect } from "next/navigation"
 
 export default function Home(){
     return(
-       <h1>
-            Landing Page
-       </h1>
+       redirect("/dashboard")
     )
 }
