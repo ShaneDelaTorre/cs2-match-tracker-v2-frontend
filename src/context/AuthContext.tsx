f62221b363d83d0938c8c8ca6baf085dcbb774bb
Shane <jc.delatorre@academy.opswerks.com>
2026-04-28 22:25:52 +0800

    refractor(auth-context): update auth context, simplified by using cookies sent by backend

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -6,43 +6,34 @@ import {
   useState,
   ReactNode,
 } from "react";
+import { logout as logoutApi} from "@/lib/endpoints";
+import { useRouter } from "next/navigation"
 
 interface AuthContextType {
   isAuthenticated: boolean;
-  isLoading: boolean;
   login: (access: string, refresh: string) => void;
-  logout: () => void;
+  logout: () => Promise<void>;
 }
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
-export function AuthProvider({ children }: { children: ReactNode }) {
-  const [authSate, setAuthState] = useState<{
-    isAuthenticated: boolean;
-    isLoading: boolean;
-  }>(() => {
-  if (typeof window === "undefined") {
-    return { isAuthenticated: false, isLoading: true };
-  }
-  const token = localStorage.getItem("access_token");
-  return { isAuthenticated: !!token, isLoading: false };
-});
+export function AuthProvider({ children, initialAuth }: { children: ReactNode, initialAuth: boolean }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(initialAuth);
+  const router = useRouter();
 
+  const login = () => {
+    setIsAuthenticated(true)
+    };
 
-  const login = (access: string, refresh: string) => {
-    localStorage.setItem("access_token", access);
-    localStorage.setItem("refresh_token", refresh);
-    setAuthState({isAuthenticated: true, isLoading: false})
-  };
-
-  const logout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("refresh_token");
-    setAuthState({isAuthenticated: true, isLoading: false})
+  const logout = async () => {
+    await logoutApi();
+    setIsAuthenticated(false);
+    router.push("/");
+    router.refresh();
   };
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated: authSate.isAuthenticated, isLoading: authSate.isLoading, login, logout }}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
